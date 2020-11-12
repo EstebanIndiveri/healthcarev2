31c330eac8db727aca0004db817e80f2d0a5bd66
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-11-12 17:42:43 -0300

    set ui

--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -123,17 +123,36 @@ const HomeScreen = () => {
                                 `}>
                                 <img height="400px" width="400px" src={require('../../imgs/uxkit/Ellipse4.png')} alt="" srcset=""/>
                                 </div>
+                                {/* blue heart */}
                                 <div style={{position:'absolute',top:'50px',left:'95px'}}>
                                     <img height="27px" width="27px" src={require('../../imgs/uxkit/emojione_blue-heart.png')} alt=""/>
                                 </div>
+                                {/* second blue heart emojione_blue-heart-2*/}
+                                <div style={{position:'absolute',top:'10px',left:'95px'}}>
+                                    <img height="27px" width="27px" src={require('../../imgs/uxkit/emojione_blue-heart.png')} alt=""/>
+                                </div>
+
+                                {/* three pink heart */}
+
                                 <div></div>
 
                                 <div className="" style={{top:'-250px',left:'9.2rem' ,height: '340px',width: '290px',backgroundColor:'white', boxShadow:'0px 7px 26px 5px rgba(0, 0, 0, 0.30)',position:'relative', zIndex:'999',borderRadius:'20px',}}>
-                                    <div style={{background:'red', borderRadius:'100%', height:'50px',width:'50px',top:'20px',left:'30px',position:'relative',display:'flex'}}>
+                                    <div style={{background:'red',boxShadow:'0px 7px 26px 5px rgba(0, 0, 0, 0.30)' ,borderRadius:'100%', height:'50px',width:'50px',top:'20px',left:'30px',position:'relative',display:'flex'}}>
                                     <div
-                                    style={{background:'#FFF', borderRadius:'100%', height:'30px',width:'30px',margin:'0 auto',justifyContent:'center',alignItems:'center',alignSelf:'center',display:'flex'}}>
+                                    style={{background:'#FFF', borderRadius:'100%', height:'35px',width:'35px',margin:'0 auto',justifyContent:'center',alignItems:'center',alignSelf:'center',display:'flex'}}>
                                         <span style={{fontSize:'10px',textAlign:'center',justifyContent:'center',margin:'0 auto',alignContent:'center',alignSelf:'center',position:'inherit'}}>70%</span>
                                     </div>
+                                    </div>
+                                    <div className=" w-100" css={
+                                        css`
+                                        position:inherit;
+                                        padding-left:1rem;
+                                        top:27px;
+                                        font-weight:bold;
+                                        font-family:'Fira Sans' !important;
+                                        letter-spacing: 3px;
+                                        `}>
+                                        Dr.Daniel
                                     </div>
                                     {/* corazones */}
                                     <div></div>
