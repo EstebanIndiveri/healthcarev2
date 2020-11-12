0dfc40a51402c735a60d8823aa544ca71c864f3e
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-11-12 00:26:23 -0300

    layout ux

--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -17,6 +17,7 @@ padding-bottom:1rem;
 
 const PresentationDesc=styled.div`
     padding:4em;
+    margin-top:8rem;
     padding-bottom:0;
     justify-content:center;
     
@@ -107,7 +108,7 @@ const HomeScreen = () => {
                             <p className="text-center animate__animated animate__fadeInLeft animate__delay-2s">More than just a medic for you and your family</p>
                         </PresentationDesc>
                             
-                            <Col css={css`@media(max-width:1000px){justify-content:center !important;padding-left:0 !important;}`} md="12" id="btnCol" style={{alignContent:"center",justifyContent:"center",alignItems:"center",display:"flex",paddingBottom:'3rem',paddingTop:'10px',marginTop:'0'}} >
+                            <Col css={css`@media(max-width:1000px){justify-content:center !important;padding-left:0 !important;}`} md="12" id="btnCol" style={{alignContent:"center",justifyContent:"center",alignItems:"center",marginBottom:'1.7rem',display:"flex",paddingTop:'10px',marginTop:'0'}} >
                             <ButtonOrange className="text-center animate__animated animate__fadeIn animate__delay-3s"> <span> Start</span></ButtonOrange>
                             </Col>
                             {/* className="animate__animated animate__fadeInLeft animate__delay-3s " */}
@@ -122,15 +123,31 @@ const HomeScreen = () => {
                                 `}>
                                 <img height="400px" width="400px" src={require('../../imgs/uxkit/Ellipse4.png')} alt="" srcset=""/>
                                 </div>
-                                <div style={{top:'-280px',left:'11.5rem' ,height: '274px',width: '200px',backgroundColor:'white', boxShadow:'0px 7px 26px 5px rgba(0, 0, 0, 0.30)',position:'relative', zIndex:'999',borderRadius:'20px'}}>
-                                
+                                <div style={{position:'absolute',top:'50px',left:'95px'}}>
+                                    <img height="27px" width="27px" src={require('../../imgs/uxkit/emojione_blue-heart.png')} alt=""/>
+                                </div>
+                                <div></div>
+
+                                <div className="" style={{top:'-250px',left:'9.2rem' ,height: '340px',width: '290px',backgroundColor:'white', boxShadow:'0px 7px 26px 5px rgba(0, 0, 0, 0.30)',position:'relative', zIndex:'999',borderRadius:'20px',}}>
+                                    <div style={{background:'red', borderRadius:'100%', height:'50px',width:'50px',top:'20px',left:'30px',position:'relative',display:'flex'}}>
+                                    <div
+                                    style={{background:'#FFF', borderRadius:'100%', height:'30px',width:'30px',margin:'0 auto',justifyContent:'center',alignItems:'center',alignSelf:'center',display:'flex'}}>
+                                        <span style={{fontSize:'10px',textAlign:'center',justifyContent:'center',margin:'0 auto',alignContent:'center',alignSelf:'center',position:'inherit'}}>70%</span>
+                                    </div>
+                                    </div>
+                                    {/* corazones */}
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
 
                                 </div>
                           </Col>
                         </Parallax>
                      
                     </Col>
-                  
+
+                  {/* segunda columna animate__animated animate__fadeInLeft animate__delay-3s */}
+
                 <Col lg="6" style={{paddingRight:'0',paddingTop:'10px'}}>
                     <div>
                         <div css={css` background-position:center; height:80vh; background-image:url('../FamilyValueshappy.png');background-position:center;background-size:contain;background-repeat:no-repeat; @media(max-width:1000px){
