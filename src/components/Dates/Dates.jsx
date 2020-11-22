7340bddb13aab09ef2923eda28b28ca8050d1463
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-11-22 02:42:03 -0300

    modify dates

--- src/components/Dates/Dates.jsx
+++ src/components/Dates/Dates.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Dates = () => {
     return ( 
-        <div>Llamar api de dates</div>
+        <table>
+            <td>
+                <tr>
+
+                </tr>
+            </td>
+        </table>
      );
 }
  
