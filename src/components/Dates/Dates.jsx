e1cdb7a6f6f56082a675e78dcaefbc2151811b62
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-11-23 01:17:07 -0300

    schedule test

--- src/components/Dates/Dates.jsx
+++ src/components/Dates/Dates.jsx
@@ -1,63 +1,187 @@
-import React, { Fragment } from 'react';
-import styled from '@emotion/styled';
-import Table from 'reactstrap/lib/Table';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+// import AppointmentPicker from "react-appointment-picker";
+import AppointmentPicker from 'appointment-picker';
 
-const Div=styled.div`
-height:100vh;
 
-`;
-
-const Dates = () => {
+class Dates extends Component{
+  state={
+    loading:false
+  };
+  addAppointmentCallback = ({ day, number, time, id }, addCb) => {
+    this.setState(
+      {
+        loading: true
+      },
+      async () => {
+        await new Promise(resolve => setTimeout(resolve, 2500));
+        console.log(
+          `Added appointment ${number}, day ${day}, time ${time}, id ${id}`
+        );
+        addCb(day, number, time, id);
+        this.setState({ loading: false });
+      }
+    );
+  };
+  addAppointmentCallbackContinuousCase = (
+    { day, number, time, id },
+    addCb,
+    params,
+    removeCb
+  ) => {
+    this.setState(
+      {
+        loading: true
+      },
+      async () => {
+        if (removeCb) {
+          await new Promise(resolve => setTimeout(resolve, 1250));
+          console.log(
+            `Removed appointment ${params.number}, day ${params.day}, time ${
+              params.time
+            }, id ${params.id}`
+          );
+          removeCb(params.day, params.number);
+        }
+        await new Promise(resolve => setTimeout(resolve, 1250));
+        console.log(
+          `Added appointment ${number}, day ${day}, time ${time}, id ${id}`
+        );
+        addCb(day, number, time, id);
+        this.setState({ loading: false });
+      }
+    );
+  };
+  removeAppointmentCallback = ({ day, number, time, id }, removeCb) => {
+    this.setState(
+      {
+        loading: true
+      },
+      async () => {
+        await new Promise(resolve => setTimeout(resolve, 2500));
+        console.log(
+          `Removed appointment ${number}, day ${day}, time ${time}, id ${id}`
+        );
+        removeCb(day, number);
+        this.setState({ loading: false });
+      }
+    );
+  };
+  render() {
+    const days = [
+      [
+        { id: 1, number: 1, isSelected: true, periods: 2 },
+        { id: 2, number: 2 },
+        null,
+        { id: 3, number: "3", isReserved: true },
+        { id: 4, number: "4" },
+        null,
+        { id: 5, number: 5 },
+        { id: 6, number: 6 }
+      ],
+      [
+        { id: 7, number: 1, isReserved: true, periods: 3 },
+        { id: 8, number: 2, isReserved: true },
+        null,
+        { id: 9, number: "3", isReserved: true },
+        { id: 10, number: "4" },
+        null,
+        { id: 11, number: 5 },
+        { id: 12, number: 6 }
+      ],
+      [
+        { id: 13, number: 1 },
+        { id: 14, number: 2 },
+        null,
+        { id: 15, number: 3, isReserved: true },
+        { id: 16, number: "4" },
+        null,
+        { id: 17, number: 5 },
+        { id: 18, number: 6 }
+      ],
+      [
+        { id: 19, number: 1 },
+        { id: 20, number: 2 },
+        null,
+        { id: 21, number: 3 },
+        { id: 22, number: "4" },
+        null,
+        { id: 23, number: 5 },
+        { id: 24, number: 6 }
+      ],
+      [
+        { id: 25, number: 1, isReserved: true },
+        { id: 26, number: 2 },
+        null,
+        { id: 27, number: "3", isReserved: true },
+        { id: 28, number: "4" },
+        null,
+        { id: 29, number: 5 },
+        { id: 30, number: 6, isReserved: true }
+      ]
+    ];
+    const { loading } = this.state;
     return (
-        <Fragment>
-        <Div>
-            <h1 className="text-center pt-4">Reserva tu turno:</h1>
-        
-            <Table responsive>
-      <thead>
-        <tr>
-          {/* <th>#</th> */}
-          <th>Table heading</th>
-          <th>Table heading</th>
-          <th>Table heading</th>
-          <th>Table heading</th>
-          <th>Table heading</th>
-          <th>Table heading</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          {/* <th scope="row">1</th> */}
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-        </tr>
-        <tr>
-          {/* <th scope="row">2</th> */}
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-        </tr>
-        <tr>
-          {/* <th scope="row">3</th> */}
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-          <td>Table cell</td>
-        </tr>
-      </tbody>
-    </Table>
-       </Div>
-       </Fragment>
-     );
+      <div>
+        <h1>Appointment Picker</h1>
+        <AppointmentPicker
+          addAppointmentCallback={this.addAppointmentCallback}
+          removeAppointmentCallback={this.removeAppointmentCallback}
+          initialDay={new Date("2018-05-05")}
+          days={days}
+          maxReservableAppointments={3}
+          alpha
+          visible
+          selectedByDefault
+          loading={loading}
+        />
+        <h1>Appointment Picker Continuous Case</h1>
+        <AppointmentPicker
+          addAppointmentCallback={this.addAppointmentCallbackContinuousCase}
+          removeAppointmentCallback={this.removeAppointmentCallback}
+          initialDay={new Date("2018-05-05")}
+          days={days}
+          maxReservableAppointments={2}
+          alpha
+          visible
+          selectedByDefault
+          loading={loading}
+          continuous
+        />
+      </div>
+    );
+  }
 }
+
+
+
+
+
+
+
+// import React, { Fragment,useState,Component } from 'react';
+// import styled from '@emotion/styled';
+// import Table from 'reactstrap/lib/Table';
+
+// const Div=styled.div`
+// height:100vh;
+
+// `;
+
+// const Dates = (props) => {
+
+//   const [counter,setCounter] = useState(0);
+//   const [name,setName] = useState("");
+
+//     return (
+//         <Fragment>
+//         <Div>
+//             <h1 className="text-center pt-4">Reserva tu turno:</h1>
+        
+//        </Div>
+//        </Fragment>
+//      );
+// }
  
-export default Dates;
+// export default Dates;
+
